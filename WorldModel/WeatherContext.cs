ae28ce48f5000be1d98fe61cc169173bee1f4acd
Alirie Gray <alirie.gray@gmail.com>
2025-09-26 18:08:28 -0700

    add world model migration

--- WorldModel/WeatherContext.cs
+++ WorldModel/WeatherContext.cs
@@ -20,27 +20,13 @@ public partial class WeatherContext : DbContext
     public virtual DbSet<Country> Countries { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("data source=(localdb)\\mssqllocaldb;initial catalog=Weather");
+        => optionsBuilder.UseSqlServer("data source=(localdb)\\mssqllocaldb;initial catalog=GoldenWeather");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<City>(entity =>
         {
-            entity.ToTable("cities");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.CountryId).HasColumnName("countryID");
-            entity.Property(e => e.Lat).HasColumnName("lat");
-            entity.Property(e => e.Lng).HasColumnName("lng");
-            entity.Property(e => e.Name)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasColumnName("name");
-            entity.Property(e => e.Population).HasColumnName("population");
-
             entity.HasOne(d => d.Country).WithMany(p => p.Cities)
-                .HasForeignKey(d => d.CountryId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_cities_countries");
         });
@@ -49,23 +35,8 @@ public partial class WeatherContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PK_country");
 
-            entity.ToTable("countries");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Iso2)
-                .HasMaxLength(2)
-                .IsUnicode(false)
-                .IsFixedLength()
-                .HasColumnName("iso2");
-            entity.Property(e => e.Iso3)
-                .HasMaxLength(3)
-                .IsUnicode(false)
-                .IsFixedLength()
-                .HasColumnName("iso3");
-            entity.Property(e => e.Name)
-                .HasMaxLength(50)
-                .IsUnicode(false)
-                .HasColumnName("name");
+            entity.Property(e => e.Iso2).IsFixedLength();
+            entity.Property(e => e.Iso3).IsFixedLength();
         });
 
         OnModelCreatingPartial(modelBuilder);
