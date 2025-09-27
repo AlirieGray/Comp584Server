c3f3bf6e08c412b85a3bc53c5945975bc5c5edef
Alirie Gray <alirie.gray@gmail.com>
2025-09-26 17:29:48 -0700

    add db scaffolding

---
+++ WorldModel/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorldModel;
+
+public partial class City
+{
+    public int Id { get; set; }
+
+    public int CountryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Population { get; set; }
+
+    public double Lat { get; set; }
+
+    public double Lng { get; set; }
+
+    public virtual Country Country { get; set; } = null!;
+}
---
+++ WorldModel/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorldModel;
+
+public partial class Country
+{
+    public int Id { get; set; }
+
+    public string Iso2 { get; set; } = null!;
+
+    public string Iso3 { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<City> Cities { get; set; } = new List<City>();
+}
